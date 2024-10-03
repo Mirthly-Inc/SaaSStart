08ba17102fb43a8ebbe346dcd6853f3ea5f46906
Anandprabhu530 <anandprabhu530@gmail.com>
2024-10-03 22:18:22 +0530

    Changing from supa to mongo

--- bin/mongo.js
+++ bin/mongo.js
@@ -1212,15 +1212,6 @@ async function configureDatabaseAndAuth(projectdir) {
   //add supabase specific code
   const utilsdir = path.join(projectdir, "utils", "supabase");
   await fs.ensureDir(utilsdir);
-
-  const clientContent = getClientContent();
-  await fs.writeFile(path.join(utilsdir, "client.ts"), clientContent);
-
-  const middlewareContent = getmiddlewareContent();
-  await fs.writeFile(path.join(utilsdir, "middleware.ts"), middlewareContent);
-
-  const serverContent = getserverContent();
-  await fs.writeFile(path.join(utilsdir, "server.ts"), serverContent);
 }
 
 function getauthcallbackContent() {
@@ -1272,119 +1263,6 @@ export async function GET(request: Request) {
 
   // If there's no code or an error occurred, redirect to an error page
   return NextResponse.redirect(\`\${origin}/auth/auth-code-error\`);
-}
-`;
-}
-
-function getserverContent() {
-  return `import { createServerClient } from "@supabase/ssr";
-import { cookies } from "next/headers";
-
-export function createClient() {
-  const cookieStore = cookies();
-
-  return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return cookieStore.getAll();
-        },
-        setAll(cookiesToSet) {
-          try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
-            );
-          } catch {
-            // The \`setAll\` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
-        },
-      },
-    }
-  );
-}
-`;
-}
-
-function getmiddlewareContent() {
-  return `import { createServerClient } from '@supabase/ssr'
-import { NextResponse, type NextRequest } from 'next/server'
-
-export async function updateSession(request: NextRequest) {
-  let supabaseResponse = NextResponse.next({
-    request,
-  })
-
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return request.cookies.getAll()
-        },
-        setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value))
-          supabaseResponse = NextResponse.next({
-            request,
-          })
-          cookiesToSet.forEach(({ name, value, options }) =>
-            supabaseResponse.cookies.set(name, value, options)
-          )
-        },
-      },
-    }
-  )
-
-  // IMPORTANT: Avoid writing any logic between createServerClient and
-  // supabase.auth.getUser(). A simple mistake could make it very hard to debug
-  // issues with users being randomly logged out.
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser()
-
-  if (
-    !user &&
-    !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/auth')
-  ) {
-    // no user, potentially respond by redirecting the user to the login page
-    const url = request.nextUrl.clone()
-    url.pathname = '/login'
-    return NextResponse.redirect(url)
-  }
-
-  // IMPORTANT: You *must* return the supabaseResponse object as it is. If you're
-  // creating a new response object with NextResponse.next() make sure to:
-  // 1. Pass the request in it, like so:
-  //    const myNewResponse = NextResponse.next({ request })
-  // 2. Copy over the cookies, like so:
-  //    myNewResponse.cookies.setAll(supabaseResponse.cookies.getAll())
-  // 3. Change the myNewResponse object to fit your needs, but avoid changing
-  //    the cookies!
-  // 4. Finally:
-  //    return myNewResponse
-  // If this is not done, you may be causing the browser and server to go out
-  // of sync and terminate the user's session prematurely!
-
-  return supabaseResponse
-}`;
-}
-
-function getClientContent() {
-  return `"use client";
-
-import { createBrowserClient } from "@supabase/ssr";
-
-export function createClient() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
 }
 `;
 }
